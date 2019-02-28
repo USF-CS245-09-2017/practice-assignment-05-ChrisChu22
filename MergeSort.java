8689503f8ae3bfb6e65c9d91b71419aa6bbddb0f
ChrisChu22 <cachu2@dons.usfca.edu>
2019-02-27 22:08:04 -0800

    I commited

--- MergeSort.java
+++ MergeSort.java
@@ -26,23 +26,28 @@ public class MergeSort implements SortingAlgorithm{
         int num1 = m - l + 1;
         int num2 = r - m;
 
+        //makes new array for left and right for original array
         int left[] = new int[num1];
         int right[] = new int[num2];
 
+        //fills the left array with values from the left side of the original array
         for (int i = 0; i < num1; ++i)
         {
             left[i] = arr[l + i];
         } //for
 
+        //fills the right array with values from the right side of the original array
         for (int j = 0; j < num2; ++j)
         {
             right[j] = arr[m + 1 + j];
         } //for
 
+        //indexes
         int ai = 0;
         int bi = 0;
         int num3 = l;
 
+        //while loops that checks the left array for values that are smaller to put into the original array and the right side as well
         while (ai < num1 && bi < num2)
         {
             if (left[ai] <= right[bi])
@@ -61,6 +66,7 @@ public class MergeSort implements SortingAlgorithm{
 
         } //while
 
+        //adds all the numbers left over in the left array
         while (ai < num1)
         {
             arr[num3] = left[ai];
@@ -68,13 +74,13 @@ public class MergeSort implements SortingAlgorithm{
             num3++;
         } //while
 
+        //adds all the numbers left over in the right array
         while (bi < num2)
         {
             arr[num3] = right[bi];
             bi++;
             num3++;
         } //while
-
 
 
     }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CS 245 (Spring, 2019) Practice Assignment 05
 
-See assignment details on Canvas.
+I did not have much trouble with this assignment. Credit to David for the quick sort code posted on slack and merge sort was simple enough.
+I reused the insertion sort code which I hope is ok. 
